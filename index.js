66bfcaf6212ad289f1b790a8bf43f21804759dde
unknown <stefano.casaretta@hotmail.it>
2025-04-24 16:27:45 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+// Redirect verso il link Pastebin
+app.get("/", (req, res) => {
+  res.redirect("https://pastebin.com/raw/YhhUGuXH");
+});
+
+// Avvio del server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
